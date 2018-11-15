6b1b3e76b934348eb8f2dc6d0b1b3cd2f938b315
Alex Rael <alprael22@gmail.com>
2018-11-15 10:17:53 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/rps/model/VonNeumann.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.rps.model;
+
+
+public class VonNeumann implements Neighborhood{
+
+  private final Location[] neighbors = {
+      new Location(-1, 0), new Location(0, -1), new Location(0, 1), new Location(1, 0)
+  };
+
+
+
+  @Override
+  public Location[] neighbors() {
+    return new Location[0];
+  }
+
+}
